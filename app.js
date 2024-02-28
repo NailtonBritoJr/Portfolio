7688c17611de2ea73c4b7dd80bb859f6f0c00613
Nailton Brito Junior <nailton.brito.2003@gmail.com>
2024-02-28 19:43:22 -0300

    commit 28/02

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const botao = document.querySelector(".botao")
 
 hamburger.addEventListener("click", () => 
   nav.classList.toggle("active"));
+
+
+botao.addEventListener("click", () => {
+    window.scroll({top: window.innerHeight, behavior: "smooth"})
+})
